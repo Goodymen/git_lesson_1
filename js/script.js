97ef5e8b97d339a61ea629ee141abc97974049b5
Goodymen <mybox495@mail.ru>
2021-12-13 13:39:40 +0300

    First lesson work

--- js/script.js
+++ js/script.js
@@ -1,166 +1,54 @@
-"use strict";
+'use strict'
 
-// ['Привет ты!', 'Да ты!', 'Здаровааа!!!',].forEach(alert);
+// Подготавливаем проект к дальнейшим урокам
+// 1) Создать HTML страницу и подключить к ней файл скрипта
+// 2) В файле скрипта создать 2 переменные (money и time), которые будут получать данные от пользователя:
+// ·      Первая будет спрашивать "Ваш бюджет на месяц?"
+// ·      Вторая "Введите дату в формате YYYY-MM-DD"
+let money = +prompt ('Какой у вас бюджет на месяц?',''),
+    time = prompt ('Введите дату в формате YYYY-MM-DD','YYYY-MM-DD');
 
+// 3) Создать объект appData, который будет содержать такие данные:
+// ·      бюджет (передаем сюда переменную из п.2)
+// ·      данные времени - timeData (передаем сюда переменную из п.2)
+// ·      объект с обязательными расходами - expenses (смотри пункт 4)
+// ·      объект с необязательными расходами - optionalExpenses (оставляем пока пустым)
+// ·      массив данных с доп. доходом - income (оставляем пока пустым)
+// ·      свойство savings (выставляем его как false)
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    saving: false,
+}
 
-// fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits')
-//   .then(response => response.json())
-//   .then(commits => alert(commits[0].author.parents));
-
-
-
-
-// var user = {
-//     firstName: "Вася",
-//     surname: "Петров"
-//   }
-  
-//   Object.defineProperty(user, "fullName", {
-//     get: function() {
-//       return this.firstName + ' ' + this.surname;
+// 4) Задать пользователю по 2 раза вопросы:
+//     “Введите обязательную статью расходов в этом месяце”
+//     “Во сколько обойдется?”
+//     Записать ответы в объект expenses в формате: 
+//     expenses: {
+//     “ответ на первый вопрос” : “ответ на второй вопрос”
 //     }
-//   });
-  
-//   alert(user.fullName); // Вася Петров
+let i = 0;
+let compulsoryExpenses, summaryExpenses;
+do {
+    compulsoryExpenses = prompt ('Введите обязательную статью расходов в этом месяце',''),
+    summaryExpenses = +prompt ('Во сколько примерно обойдется','рублей');
+    appData.expenses[compulsoryExpenses] = summaryExpenses;
+    i++;
+} while (i < 2);
 
-// var user = {
-//     firstName: "Вася",
-//     surname: "Петров",
-  
-//     get fullName() {
-//       return this.firstName + ' ' + this.surname;
-//     },
-  
-//     set fullName(value) {
-//       var split = value.split(' ');
-//       this.firstName = split[0];
-//       this.surname = split[1];
-//     }
-//   };
-  
-//   alert( user.fullName ); // Вася Петров (из геттера)
-  
-//   user.fullName = "Петя Иванов";
-//   alert( user.firstName ); // Петя  (поставил сеттер)
-//   alert( user.surname ); // Иванов (поставил сеттер)
-//   alert( user.fullName );
+// 5) Вывести на экран пользователя бюджет на 1 день (брать месяц за 30 дней, использовать alert)
+alert (`Бюджет на 1 день составит ${(appData.budget / 30).toFixed(2)} рублей`);
 
+// 6) Проверить, чтобы все работало без ошибок в консоли
+console.log (appData);
 
+// 7) Создать свой репозиторий на GitHub и поместить туда папку с первым заданием
+// Вопросы к этому заданию
+// Сколько типов данных существует в JS?
+// Как вывести информацию в консоль?
+// Какая функция операторов || и &&?
 
-// let promise = fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
-//   .then(response => response.json())
-//   .then(commits => alert(commits[0]));
-
-
-// fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits')
-//   .then(response => response.json())
-//   .then(commits => alert(commits[0].author.login));
-
-
-
-// async function getResponse() {
-//     let response = await fetch('https://jsonplaceholder.typicode.com/photos');
-//     let content = await response.json();
-//     content = content.splice (0, 10);
-
-//     let list = document.querySelector ('.posts')
-
-
-//     for (let key in content) {
-//         list.innerHTML += `
-//             <li class="post">
-//                 <h4>${content[key].title}</h4>
-//                 <img src="${content[key].url}" width="100" alt="">
-//             </li>
-//         `
-//     }
-// }
-// getResponse()
-
-
-// async function getResponse() {
-//     let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200');
-//     let content = await response.json();
-//     console.log(content.results);
-// }
-
-// getResponse()
-
-
-// fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
-//   .then(res => res.json())
-//   .then(content => console.log(content.results));
-
-// // Metod 1
-// str = "Hello";
-// console.log (str);
-
-// // Metod 2
-// console.log ( str.slice(0, 5) );
-
-// // Metod 3
-// console.log ( str.substring(0, 5) );
-
-// // Metod 4
-// console.log ( str.substr(0, 5) );
-
-// // Metod 5
-// console.log ( 'Hello' );
-
-// Metod 6
-// let strNew = str.split (' ', 4)
-// console.log ( strNew[0] )
-// console.log ( strNew[1] )
-// console.log ( strNew[2] )
-
-
-
-// let srtNew;
-// function splitString (n) {
-//   for (let i = 0; i < n; i++) {
-//     strNew += str[i]
-//   }
-// }
-// splitString (5);
-
-// for (let char of str) {
-//   if (char === 'l') {
-//     char = 'L'}
-//   console.log ( char )
-// }
-
-
-
-
-
-
-
-
-
-// let obj = {
-//     name: 'Andrey',
-//     age: 36,
-//     email: 'mybox@mail.ru',
-//     telefon: '+79099638531'
-// };
-
-// obj.job = 'Freelancer in Life';
-
-// console.log (obj["name"]);
-
-// for (let key in obj) {
-//     console.log (obj[key]);
-// }
-
-let obj = {};
-
-obj.name = 'Andrey';
-obj.age = 36;
-obj.job = 'freelancer';
-
-console.log (obj);
-
-
-// for (let i = 0; i < obj.length; i++) {
-//     console.log (obj(i))
-// }
